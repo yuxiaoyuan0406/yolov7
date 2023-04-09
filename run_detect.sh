0a8fa0b4ece86ae84cb3954c1de3280abb4fd657
yuxiaoyuan0406 <973971990@qq.com>
2023-04-09 16:16:37 +0800

    files for train and detect test

--- run_detect.sh
+++ run_detect.sh
@@ -3,23 +3,30 @@
 py=/home/dennis/anaconda3/envs/rolov7/bin/python
 f=/home/dennis/Programs/yolov7/detect.py
 
-name=yolov7-people5
+name=yolov7-people7
 train_dir=./runs/train
-# source=/home/dennis/Programs/yolov7/output/test-2023-03-09-221724-clip-1-add-frame-num-2023-03-10-161648.mp4
-source=/home/dennis/Programs/yolov7/output/test-2023-03-13-214015-clip-1-add-frame-num-2023-03-13-221423.mp4
+source=/home/dennis/Programs/yolov7/output/test-2023-03-09-221724-clip-1-add-frame-num-2023-03-10-161648.mp4
+# source=/home/dennis/Programs/yolov7/output/test-2023-03-13-214015-clip-1-add-frame-num-2023-03-13-221423.mp4
 size=640
 conf=0.4
 # view_image=--view-img
 save_txt=--save-txt
 # save_conf=--save-conf
 # nosave=--nosave
+# exist_ok=--exist-ok
 
 
+run(){
 $py $f \
 --weights $train_dir/$name/weights/best.pt \
 --source $source \
 --img-size $size \
 --conf-thres $conf \
-$view_image $save_txt $save_conf $nosave \
+$view_image $save_txt $save_conf $nosave $exist_ok \
 --name $name-detect
+}
+
+run
+source=/home/dennis/Programs/yolov7/output/test-2023-03-13-214015-clip-1-add-frame-num-2023-03-13-221423.mp4
+run
 
--- run_train.sh
+++ run_train.sh
@@ -8,18 +8,18 @@ name=yolov7-people
 workers=8
 device=0
 batch_size=32
-data=/home/dennis/Programs/yolov7/data/coco-people-47k.yaml
+data=/home/dennis/Programs/yolov7/data/coco-people-94k.yaml
 width=640
 height=640
 cfg=/home/dennis/Programs/yolov7/cfg/training/yolov7-tiny-people.yaml
 weights=\'\'
 hyp=/home/dennis/Programs/yolov7/data/hyp.scratch.p5.yaml
-epochs=50
+epochs=300
 save_period=2
 
 $py $f \
 --workers $workers \
---device $device\
+--device $device \
 --batch-size $batch_size \
 --data $data \
 --img $width $height \
